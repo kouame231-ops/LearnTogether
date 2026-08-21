25dbc302fdf257571f44a868f37678eec35a723b
kouame231-ops <erickouame.cri@gmail.com>
2026-08-21 18:47:25 +0000

    Ajout du @Preview

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign                
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -87,7 +87,7 @@ fun ComposeArticleScreen(modifier: Modifier = Modifier) {
 }
 
 // Aperçu (Preview) : permet de voir le résultat dans Android Studio
-// sans avoir besoin de lancer l'application sur un téléphone/émulateur
+// sans avoir besoin de lancer l'application sur un téléphone émulateur
 @Preview(showBackground = true)
 @Composable
 fun ComposeArticleScreenPreview() {
